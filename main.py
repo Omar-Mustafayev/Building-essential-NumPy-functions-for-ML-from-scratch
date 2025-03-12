af97984c3891fe9a3a2c67f1980a00f72697b745
Omar Mustafayev <omarmustafayev284@gmail.com>
2025-03-12 20:46:11 +0400

    Shape func

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import numpy as np
 
 print("Hello, world!")
+
+x = [[1],[2],[3]]
+y = [4,5,6]
+A = [[1,2,3],[4,5,6]]
+B = [[1,2],[3,4],[5,6]]
+
+def shape(array):
+    num_rows = len(array)
+    try:
+        num_columns = len(array[0])
+    except:
+        num_columns = None
+    return num_rows, num_columns
+    
+print(shape(A))
+print(np.shape(A))
+print(shape(x))
+print(np.shape(np.transpose(x)))
+print(x)
